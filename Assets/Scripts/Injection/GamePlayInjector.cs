175369c4df1b39457cb42b6a534b2c4cb3795c12
RenSalazar <salazarrenelie@gmail.com>
2019-08-16 20:23:53 +0800

    [Part 2] Imported test character animation frames and tileset

--- Assets/Scripts/Injection/GamePlayInjector.cs
+++ Assets/Scripts/Injection/GamePlayInjector.cs
@@ -1,20 +1,55 @@
-﻿using NaughtyAttributes;
+﻿using Audio;
+using Character.Ground;
+using GamePlayInput;
+using NaughtyAttributes;
 using UnityEngine;
 using Zenject;
 
-public class GamePlayInjector : MonoInstaller<GamePlayInjector>
-{
+namespace Injection {
 
-    [SerializeField]
-    [Required]
-    private AudioModel m_modelAudio;
 
-    public override void InstallBindings()
+    public class GamePlayInjector : MonoInstaller<GamePlayInjector>
     {
-        Container.Bind<AudioModel.Getter>().FromInstance(m_modelAudio);
-        Container.Bind<AudioModel.Setter>().FromInstance(m_modelAudio);
+
+        [SerializeField]
+        [Required]
+        private AudioModel m_modelAudio;
+
+
+        [SerializeField]
+        [Required]
+        private PlayerGroundModel m_modelPlayerGround;
+        [SerializeField]
+        [Required]
+        private GamePlayInputController m_gamePlayInputController;
+
+        public override void InstallBindings()
+        {
+            BindModelAudio();
+
+            BindModelPlayer();
+        }
+
+        private void BindModelAudio()
+        {
+            Container.Bind<AudioModel.Getter>().FromInstance(m_modelAudio);
+            Container.Bind<AudioModel.Setter>().FromInstance(m_modelAudio);
+        }
+
+        private void BindModelPlayer()
+        {
+            //Player Ground
+            Container.Bind<PlayerGroundModel.Getter>().FromInstance(m_modelPlayerGround);
+            Container.Bind<PlayerGroundModel.Setter>().FromInstance(m_modelPlayerGround);
+
+            //Player Input
+            BaseInputModel baseInputModel = m_gamePlayInputController.GetBaseInput();
+            LogUtil.PrintInfo(gameObject, GetType(), "base input is: " + baseInputModel.GetType());
+            Container.Bind<BaseInputModel>().FromInstance(baseInputModel);
+
+        }
+
 
     }
-
 
 }
