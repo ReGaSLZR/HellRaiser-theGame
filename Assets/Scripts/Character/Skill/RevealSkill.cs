9648f1189a7d8b5edcdc55c3f3884916da1b5929
Renelie Salazar <renelie_s@pretia.co.jp>
2019-11-17 22:23:58 +0900

    Created Prefabs for Beggar/Trapper Girl and Boy Coded Reveal Skill

---
+++ Assets/Scripts/Character/Skill/RevealSkill.cs
@@ -0,0 +1,35 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+namespace Character.Skill
+{
+
+    public class RevealSkill : BaseSkill
+    {
+
+        [Header("----- Child variables -----")]
+        [Required]
+        [SerializeField] private GameObject m_objectToReveal;
+
+        private void OnEnable()
+        {
+            if(m_objectToReveal != null)
+            {
+                m_objectToReveal.SetActive(false);
+            }
+        }
+
+        protected override void OnSkillStart()
+        {
+            if ((m_objectToReveal != null) &&
+                !m_objectToReveal.activeInHierarchy)
+            {
+                base.OnSkillStart();
+                m_objectToReveal.SetActive(true);
+            }
+            
+        }
+
+    }
+
+}
