0ea823201e868f9687b1ad3109cad29bbd0c4f2e
Renelie Salazar <renelie_s@pretia.co.jp>
2019-12-02 10:29:41 +0900

    Added About page contents and Hyperlink opening

---
+++ Assets/Scripts/Common/UI/GraphicsHyperlink.cs
@@ -0,0 +1,38 @@
+﻿namespace Common.UI
+{
+
+    using UniRx;
+    using UniRx.Triggers;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class GraphicsHyperlink : MonoBehaviour
+    {
+
+        [SerializeField] private MaskableGraphic[] m_graphicsToClick;
+        [SerializeField] private string m_link;
+
+        private void Awake()
+        {
+            if ((m_graphicsToClick.Length == 0) || (m_link == null))
+            {
+                Destroy(this);
+            }
+        }
+
+        private void Start()
+        {
+            foreach (MaskableGraphic graphic in m_graphicsToClick)
+            {
+                if (graphic != null)
+                {
+                    graphic.OnPointerClickAsObservable()
+                        .Subscribe(_ => Application.OpenURL(m_link))
+                        .AddTo(this);
+                }
+            }
+        }
+
+    }
+
+}
