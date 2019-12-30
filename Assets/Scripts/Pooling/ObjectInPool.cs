5247ad9db7cfa25018a6b21a347de99aaef400b5
Renelie Salazar <renelie_s@pretia.co.jp>
2019-12-30 19:25:41 +0900

    Coded Object-Pooling classes (ObjectInPool and ObjectPooler)

---
+++ Assets/Scripts/Pooling/ObjectInPool.cs
@@ -0,0 +1,47 @@
+﻿namespace Pooling
+{
+
+    using UnityEngine;
+    using Utils;
+    using System.Collections;
+
+    public class ObjectInPool : MonoBehaviour
+    {
+
+        private int m_index;
+        private ObjectPooler m_pooler;
+
+        public void SetUp(int index, ObjectPooler pooler)
+        {
+            m_index = index;
+            m_pooler = pooler;
+        }
+
+        public void PutBackToPool()
+        {
+            if (m_pooler == null)
+            {
+                LogUtil.PrintWarning(GetType(), "PutBackToPool():" +
+                    " Missing ObjectPooler. Did you forget to call SetUp()?");
+            }
+            else
+            {
+                m_pooler.RecycleInPool(m_index);
+            }
+            
+        }
+
+        public void PutBackToPool(float delay)
+        {
+            StartCoroutine(CorBackToPool(delay));
+        }
+
+        private IEnumerator CorBackToPool(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            PutBackToPool();
+        }
+
+    }
+
+}
