d54e6662517af19e0efa75b40ab91d9d1c6d08ba
Renelie Salazar <renelie_s@pretia.co.jp>
2020-03-04 00:19:32 +0900

    Coded CutsceneTrigger

---
+++ Assets/Scripts/GamePlay/Base/CutsceneTrigger.cs
@@ -0,0 +1,73 @@
+﻿namespace GamePlay.Base
+{
+
+    using NaughtyAttributes;
+    using Input;
+
+    using System.Collections;
+
+    using UnityEngine;
+    using UnityEngine.Playables;
+
+    using Zenject;
+
+    public class CutsceneTrigger : BaseTrigger
+    {
+
+        [Inject]
+        private readonly BaseInputModel m_modelInput;
+
+        [Required]
+        [SerializeField]
+        private PlayableDirector m_playableCutscene;
+
+        [SerializeField]
+        private bool m_isFrozenInput;
+
+        [Space]
+
+        [SerializeField]
+        private BaseTrigger m_chainedTrigger;
+
+        [Space]
+
+        [SerializeField]
+        [Range(0.1f, 10f)]
+        private float m_delayBeforeDestroy = 0.1f;
+
+        public override void Execute()
+        {
+            StopAllCoroutines();
+            StartCoroutine(CorPlayCutscene());
+        }
+
+        private IEnumerator CorPlayCutscene()
+        {
+            m_playableCutscene.Play();
+
+            if (m_isFrozenInput)
+            {
+                m_modelInput.DisableControls();
+            }
+
+            yield return new WaitForSeconds((float)m_playableCutscene.duration);
+
+            if (m_isFrozenInput)
+            {
+                m_modelInput.EnableControls();
+            }
+
+            if ((m_chainedTrigger != null))
+            {
+                m_chainedTrigger.Execute();
+            }
+
+            yield return new WaitForSeconds(m_delayBeforeDestroy);
+
+            Destroy(this.gameObject);
+            
+        }
+
+    }
+
+}
